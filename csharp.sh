b29450225c16dd4fea65efd9d7fcb68cd4e6790d
rustleupchef <rustle.up.chef@gmail.com>
2025-08-15 08:22:04 -0500

    fixed curl and added gitignore

--- csharp.sh
+++ csharp.sh
@@ -1,4 +1,4 @@
-curl https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.sh | bash
+curl https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.sh -o dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --install-dir ~/Library/DotNet
 echo 'export PATH="$PATH:$HOME/Library/DotNet"' >> ~/.zshrc
