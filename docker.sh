6bf84c1f456e1c937d3b35ae0655c57e4f4af462
rustleupchef <rustle.up.chef@gmail.com>
2025-09-23 15:48:12 -0500

    fixed syntax error

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 curl https://desktop.docker.com/mac/main/arm64/124339/Docker.dmg -o Docker.dmg
 mkdir -p ~/Desktop/Applications
-ehco "Please open the Docker.dmg file and move it into the Applications folder on your Desktop."
+echo "Please open the Docker.dmg file and move it into the Applications folder on your Desktop."
